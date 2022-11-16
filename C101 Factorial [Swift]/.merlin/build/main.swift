27376c5e3a0b9935dc58ebf60fb2654f9e4ae96f
Will Turner <william.turner@student.allenisd.org>
2022-11-16 12:12:25 -0600

    initial commit

---
+++ C101 Factorial [Swift]/.merlin/build/main.swift
@@ -0,0 +1,39 @@
+// ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+let n = 5
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+// ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧
+import Swift
+import Foundation
+
+func factorial(n: Int) -> Int {
+    var result = 1
+    if (n > 0) {
+        for i in 1...n {
+            result *= i
+        }
+    }
+    return result
+}
+func resultPrint(array: [Int]) {
+    let arrayCount = array.count - 1
+    for i in 0...arrayCount {
+        if array[arrayCount-i] != 1 {
+            print("Non-terminal case, returning \(array[arrayCount-i]).")
+        } else {
+            print("Terminal case, returning 1.")
+        }
+    }
+    print("The final result is: \(array[0]).")
+}
+var x = n
+var resultArray : [Int] = []
+while x != 0 {
+    print("Calculating factorial(\(x))...")
+    let result = factorial(n: x)
+    resultArray.append(result)
+    if result == 1 {
+        resultPrint(array: resultArray)
+    }
+    x -= 1
+}
---
+++ C102 Fibonacci [Swift]/main.swift
@@ -0,0 +1,20 @@
+// ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+let n = 7
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+// ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧
+import Swift
+import Foundation
+
+func fib(int: Int) -> Int {
+    print("Calculating fibonacci(\(int))...")
+    if (int <= 2){
+        print("Terminal case, returning 1.")
+        return 1
+    } else {
+        let result = fib(int: int - 1) + fib(int: int-2)
+        print("Non-terminal case, returning \(result).")
+        return result
+    }
+}
+print("The final result is: \(fib(int: n)).")
